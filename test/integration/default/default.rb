3892aeeb0f94d36a5e02e22306fad95c8d3cb6ae
Zopanix <zopanix@gmail.com>
2018-11-19 10:49:45 -0500

    Added new tests on configuration options

--- test/integration/default/default.rb
+++ test/integration/default/default.rb
@@ -11,3 +11,23 @@ control "sssd - #{os.name} #{os.release} - 01" do
     end
   end
 end
+
+control "sssd - #{os.name} #{os.release} - 02" do
+  title 'Ensure configuration has been properly defined'
+  lines = [
+    'dyndns_refresh_interval = 30',
+    'use_fully_qualified_names = False',
+    'entry_cache_group_timeout = 200',
+    ', corp.dazzlingwrench.internal',
+    'domain/corp.dazzlingwrench.internal',
+    'id_provider = ad',
+    'max_id = 0',
+    'enumerate = False',
+    'services = nss, pam'
+  ]
+  lines.each do |line|
+    describe file('/etc/sssd/sssd.conf') do
+      its('content') { should match(%r{#{line}}) }
+    end
+  end
+end
